8b9ca4f41be0431ccbffaf86a7b14f4c20fa4355
upendar-bandam <bandamupendar4868@gmail.com>
2025-12-17 17:53:29 +0530

    Update customerDetails API endpoint URL

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.post('/customer/customerDetails', async (req, res) => {
     try {
       const data = req.body;
       // const response = await axios.post('https://backend.hyderabad-packers-movers.in/customer/customerDetails', data);
-      const response = await axios.post('http://51.21.244.112:8080/customer/customerDetails', data);
+      const response = await axios.post('http://13.62.128.188:8080/customer/customerDetails', data);
       // Send the JSON response from Spring Boot to the frontend
       res.status(200).json(response.data);
     } catch (error) {
